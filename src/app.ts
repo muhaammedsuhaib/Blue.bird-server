847a74be00098be36aff8622817d7be7a1cc8d86
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-11-02 11:24:40 +0530

    feat: set up cors origin

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 import authRoutes from "./modules/auth/auth.routes";
 import connectDB from "./config/db";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 
 dotenv.config();
 
@@ -11,10 +11,23 @@ const app = express();
 
 app.use(cookieParser());
 
-const allowedOrigin = process.env.ALLOWED_ORIGIN || "";
+const allowedOrigins: string[] = (process.env.ALLOWED_ORIGIN || "")
+  .split(",")
+  .map((origin) => origin.trim());
 
-const corsOptions: cors.CorsOptions = {
-  origin: allowedOrigin,
+const corsOptions: CorsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    console.log("Request Origin:", origin);
+
+    if (allowedOrigins.includes(origin as string) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
